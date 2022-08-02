4349b217e9dd2824e6928fc9a0ce82f8cec8a5ee
Abdo Amin <abdo.amin9991@gmail.com>
2022-08-02 11:49:50 +0200

    soem styling

--- state-manager-react/src/App.jsx
+++ state-manager-react/src/App.jsx
@@ -6,11 +6,40 @@ function App() {
   const dispatch = useDispatch();
   const count = useSelector((state) => state.count);
   return (
-    <div>
-      <h1>Count: {count}</h1>
-      <button onClick={() => dispatch({ type: "INCREMENT", amount: 1 })}>
-        Increment
-      </button>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <h1 style={{ color: "#535bf2" }}>Redux-like!</h1>
+      <strong style={{ fontSize: "36px", marginBottom: "30px" }}>
+        Count: {count}
+      </strong>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "10px",
+        }}
+      >
+        <button onClick={() => dispatch({ type: "INCREMENT", amount: 1 })}>
+          Increment
+        </button>
+        <button
+          onClick={() =>
+            count > 0 && dispatch({ type: "DECREMENT", amount: 1 })
+          }
+        >
+          Decrement
+        </button>
+      </div>
     </div>
   );
 }
