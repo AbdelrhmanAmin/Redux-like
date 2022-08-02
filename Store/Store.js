b18ab088308fdaea74f72b4d07a1642ebfbf3a53
Abdo Amin <abdo.amin9991@gmail.com>
2022-08-02 15:11:31 +0200

    Add todo example + only publish to the affected subscribers + subscribe by slice (key) in the state (#2)
    * add todo and counter + subscribe from useSelector
    
    * remove proxy
    
    * fixes
    
    * only publish to affected subscribers
    
    * memoize and refactor code

--- Store/Store.js
+++ Store/Store.js
@@ -8,41 +8,31 @@ class Store {
       throw new Error("Reducer is required");
     }
     this.reducer = reducer;
-    // A proxy to supervise the state and publish updates to the subscribers.
-    this.state = new Proxy(this.state, {
-      set: (target, key, value) => {
-        target[key] = value;
-        this.eventsManager.publish("stateChange", {
-          key,
-          value,
-        });
-        return true;
-      },
-    });
   }
   getState = () => {
     return this.state;
   };
   dispatch = ({ type, ...rest }) => {
-    let self = this;
     const action = { type, ...rest };
     const newState = this.reducer(this.state, action);
     // only update the affected slots in state.
     const diff = findDiff(this.state, newState);
     Object.keys(diff).forEach((key) => {
       this.state[key] = newState[key];
+      // publish ONLY to the affected subscribers e.g (count, items, ...etc)
+      this.eventsManager.publish(key, newState[key]);
     });
   };
   // to avoid having to use this.store.eventsManager.method_name()
-  subscribe(action, callback) {
+  subscribe = (action, callback) => {
     return this.eventsManager.subscribe(action, callback);
-  }
-  unsubscribe(action, callback) {
+  };
+  unsubscribe = (action, callback) => {
     return this.eventsManager.unsubscribe(action, callback);
-  }
-  publish(action, payload) {
+  };
+  publish = (action, payload) => {
     return this.eventsManager.publish(action, payload);
-  }
+  };
   // ------------------------------------------------------------
 }
 
--- state-manager-react/src/App.jsx
+++ state-manager-react/src/App.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import { useStore } from "./components/Provider";
+import Counter from "./Counter";
 import { useDispatch, useSelector } from "./hooks";
+import Todo from "./TODO";
 
 function App() {
-  const dispatch = useDispatch();
-  const count = useSelector((state) => state.count);
   return (
     <div
       style={{
@@ -17,28 +17,15 @@ function App() {
       }}
     >
       <h1 style={{ color: "#535bf2" }}>Redux-like!</h1>
-      <strong style={{ fontSize: "36px", marginBottom: "30px" }}>
-        Count: {count}
-      </strong>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "center",
-          gap: "10px",
-        }}
-      >
-        <button onClick={() => dispatch({ type: "INCREMENT", amount: 1 })}>
-          Increment
-        </button>
-        <button
-          onClick={() =>
-            count > 0 && dispatch({ type: "DECREMENT", amount: 1 })
-          }
-        >
-          Decrement
-        </button>
+      <div style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "20px",
+      }}>
+        <Counter />
+        <Todo />
       </div>
     </div>
   );
