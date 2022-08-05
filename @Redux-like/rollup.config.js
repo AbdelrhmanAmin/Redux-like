5f292d24d48734ca97b252413282b77d614abcec
Abdo Amin <abdo.amin9991@gmail.com>
2022-08-05 17:55:03 +0200

    idk but it worked

--- @Redux-like/rollup.config.js
+++ @Redux-like/rollup.config.js
@@ -1,28 +1,31 @@
 import babel from "rollup-plugin-babel";
-import resolve from "@rollup/plugin-node-resolve";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 import external from "rollup-plugin-peer-deps-external";
 import { terser } from "rollup-plugin-terser";
+import pkg from "./package.json";
+const extensions = [".js", ".jsx"];
 
 export default [
   {
     input: "./index.js",
-    output: [
-      {
-        file: "./dist/index.js",
-        format: "cjs",
+    external: Object.keys(pkg.peerDependencies || {}).concat("react-dom"),
+    output: {
+      format: "umd",
+      name: "Redux-like",
+      file: "./dist/redux-like.js",
+      globals: {
+        react: "React",
+        "react-dom": "ReactDOM",
       },
-      {
-        file: "./dist/index.es.js",
-        format: "es",
-      },
-    ],
+    },
     plugins: [
+      nodeResolve({ extensions }),
       babel({
         exclude: "node_modules/**",
         presets: ["@babel/preset-react"],
+        extensions,
       }),
       external(),
-      resolve(),
       terser(),
     ],
   },
