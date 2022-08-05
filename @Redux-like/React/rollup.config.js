29d9fef7cde53c6cd7e59dcd0fc5a5404985e5ce
Abdo Amin <abdo.amin9991@gmail.com>
2022-08-05 19:35:37 +0200

    React rollup

--- @Redux-like/React/rollup.config.js
+++ @Redux-like/React/rollup.config.js
@@ -1,3 +1,7 @@
+import babel from "@rollup/plugin-babel";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
 export default {
   input: "./index.js",
   output: {
@@ -6,4 +10,13 @@ export default {
     file: "./dist/redux-like.js",
     sourcemap: "inline",
   },
+  plugins: [
+    nodeResolve({
+      extensions: [".js", ".jsx"],
+    }),
+    babel({
+      presets: ["@babel/preset-react"],
+    }),
+    commonjs(),
+  ],
 };
