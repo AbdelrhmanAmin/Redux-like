100db176ff4c8419d62bc17d822eddd1b536d7bc
Abdo Amin <abdo.amin9991@gmail.com>
2022-08-14 18:53:53 +0200

    rm

--- examples/src/TimeTraverler.jsx
+++ examples/src/TimeTraverler.jsx
@@ -1,16 +1,16 @@
 import { useSelector } from "redux-like-react";
 
 const TimeTraveler = () => {
-  const history = useSelector((state) => state.history);
+  // const history = useSelector((state) => state.history);
   return (
     <div>
       <strong style={{ fontSize: "36px" }}>Time Traveler</strong>
       <ul>
-        {Object.entries(history).map(([time, entry], i) => (
+        {/* {Object.entries(history).map(([time, entry], i) => (
           <li key={i}>
             {time}-{entry.action}
           </li>
-        ))}
+        ))} */}
       </ul>
     </div>
   );
--- examples/src/reducers/index.js
+++ examples/src/reducers/index.js
@@ -2,32 +2,9 @@ import { combineReducers } from "redux-like-core";
 import counterReducer from "./counterReducer";
 import itemsReducer from "./itemsReducer";
 
-export const historyReducer = (state = {}, action) => {
-  if (!state.history) return state;
-  console.log({ state });
-  const history = state.history || {};
-  if (action.type === "HISTORY/TRAVEL") {
-    return {
-      ...history[action.payload.time].currentState,
-      history,
-    };
-  } else {
-    history[Date.now()] = {
-      // the current state the store
-      currentState: state,
-      action: action.type,
-    };
-  }
-  return {
-    ...state,
-    history,
-  };
-};
-
 const rootReducer = combineReducers({
   count: counterReducer,
   items: itemsReducer,
-  history: historyReducer,
 });
 
 export default rootReducer;
